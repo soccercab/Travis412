e4d3fba7a79235891809ba72cf6cedf18f4140a7
Connor Braude <soccercab@yahoo.com>
2021-12-12 23:28:41 -0500

    Mult and Divide

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -8,4 +8,17 @@ public class SimpleCalculator {
 	public int sub(int a, int b) {
 		return a-b;
 	}
+
+	public int mul(int a, int b) {
+		return a*b;
+	}
+
+	public int div(int a, int b) {
+		if (a/b != 0){
+			return a/b;
+		}
+		else{
+			return -1;
+		}
+	}
 }
