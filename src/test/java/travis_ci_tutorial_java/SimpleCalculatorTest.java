63a1c532ac05c154651247a0f0098034b099b849
Connor Braude <soccercab@yahoo.com>
2021-12-12 23:24:22 -0500

    Sub

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,4 +11,8 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
+	public void testSub() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.sub(5, 3), 2);
+	}
 }
